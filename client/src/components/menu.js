8247edafa8b8cf756d07a99a01af3ac3239bcdb5
Therin Mody <therin.mody@edu.sait.ca>
2022-02-01 11:41:50 -0800

    Began building menu component, going to break for lunch

---
+++ client/src/components/menu.js
@@ -0,0 +1,31 @@
+//import {Link} from '@reach/router';
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Menu = () => {
+    return (
+        <MenuContainer>
+            <MenuTitle>
+
+            </MenuTitle>
+
+            <MenuContent>
+
+            </MenuContent>
+        </MenuContainer>
+    );
+};
+
+export default Menu;
+
+const MenuContainer = styled.div({
+    
+});
+
+const MenuTitle = styled.div({
+    
+});
+
+const MenuContent = styled.div({
+    
+});
